3f899d5373d939a6181ab93d7cbb7cf46bd09a5d
sanyu1810-droid <sanyuktakadam1810@gmail.com>
2025-06-20 16:21:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Toggle scroll class on navbar
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 60) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
